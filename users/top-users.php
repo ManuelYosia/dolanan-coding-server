f610bad8f0c4802f4d5ac5cd8c87a5c0330399ad
Aeshiru <ferryjuliardi1305@gmail.com>
2024-12-22 12:14:56 +0700

    change code for leaderboard rank

--- users/top-users.php
+++ users/top-users.php
@@ -12,7 +12,7 @@ header(
 function getTopUsers( $conn) {
     $response = [];
     $data = [];
-    $sql1 = "SELECT username FROM users WHERE stars > 0 ORDER BY stars DESC  LIMIT 10";
+    $sql1 = "SELECT username,stars FROM users WHERE stars > 0 ORDER BY stars DESC  LIMIT 10";
 
     $result = $conn->query($sql1);
 
